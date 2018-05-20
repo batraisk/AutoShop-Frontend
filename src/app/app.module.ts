ebe30651d35a69b9bfc9a93e931232da68296913
batraisk <AndreyK-ov@yandex.ru>
2018-05-20 21:23:11 +0300

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
+    HeaderComponent,
   ],
   imports: [
     AppRoutingModule,
